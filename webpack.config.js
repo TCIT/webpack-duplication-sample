b5ff66bba8f51d3bec9d6b6969e9a9d91160fb6d
Tomás Charad <tomas.charad@gmail.com>
2017-07-21 21:24:11 -0400

    Running builds and adding sample duplicated file

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   // resolveLoader: {root: path.join(__dirname, 'node_modules')},
   entry: ['./react/app'],
   output: {
-    path: path.join(__dirname, 'public/bundles/'),
+    path: path.join(__dirname, '/bundles/'),
     filename: '[name].[hash].bundle.js',
     publicPath: '/'
   },
